dc31303ee6700f24b1dd573ad7efc6cf00da7b32
Priyankar <99131030+Sushitrashhhh@users.noreply.github.com>
2025-11-05 02:41:52 +0530

    move files

--- architecture.py
+++ architecture.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torchvision import datasets, transforms, models
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader 
 import os
 import copy
 from torch.optim.lr_scheduler import ReduceLROnPlateau
